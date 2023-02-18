7c5f6b0bce1b05208c29ee13792d282f679a17c1
lalo456 <83661186+lalo456@users.noreply.github.com>
2023-02-18 16:34:58 -0600

    ABR
    test

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiamensaje, 3500)
+
+function cambiamensaje(){
+document.getElementById("loading-message").innerText="Enviado!"
+}
+setTimeout(function (){
+    alert("Aca esta tu abrazo CTM <3")
+},3600
+)
